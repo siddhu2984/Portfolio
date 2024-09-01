f80bf61ddaa6ff1695d4f2efe9ee394e33a1d76a
siddharth <98523530+siddhu2984@users.noreply.github.com>
2024-09-01 20:20:52 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -40,22 +40,25 @@
 
 
     //===== Section Menu Active
-
     var scrollLink = $('.page-scroll');
     // Active link switching
     $(window).scroll(function() {
         var scrollbarLocation = $(this).scrollTop();
-
+    
         scrollLink.each(function() {
-
-            var sectionOffset = $(this.hash).offset().top - 73;
-
-            if (sectionOffset <= scrollbarLocation) {
-                $(this).parent().addClass('active');
-                $(this).parent().siblings().removeClass('active');
+    
+            var target = $(this.hash);
+            if (target.length) { // Ensure the target exists
+                var sectionOffset = target.offset().top - 73;
+    
+                if (sectionOffset <= scrollbarLocation) {
+                    $(this).parent().addClass('active');
+                    $(this).parent().siblings().removeClass('active');
+                }
             }
         });
     });
+
 
 
 
